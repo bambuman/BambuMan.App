5bad58bd43637e32d94d6f2bc117869f65319db3
Noismaster <fredi.at@gmail.com>
2026-07-10 17:51:22 +0300

    pla pure Apricot match

--- src/BambuMan.Shared/ExternalFilamentMatcher.cs
+++ src/BambuMan.Shared/ExternalFilamentMatcher.cs
@@ -216,6 +216,7 @@ namespace BambuMan.Shared
             if (info.MaterialVariantIdentifier.EqualsCI("A00-P1")) query = externalFilaments.Where(x => x.Id.EqualsCI("bambulab_pla_pink_1000_175_n")).AsQueryable();
             if (info.MaterialVariantIdentifier.EqualsCI("A19-P00")) query = externalFilaments.Where(x => x.Id.EqualsCI("bambulab_pla_puremilkypink_1000_175_n")).AsQueryable();
             if (info.MaterialVariantIdentifier.EqualsCI("A19-B00")) query = externalFilaments.Where(x => x.Id.EqualsCI("bambulab_pla_silk+babyblue_1000_175_n")).AsQueryable();
+            if (info.MaterialVariantIdentifier.EqualsCI("A19-A00")) query = externalFilaments.Where(x => x.Id.EqualsCI("bambulab_pla_pureapricot_1000_175_n")).AsQueryable();
 
             if (info.DetailedFilamentType.EqualsCI("PLA Basic") && color.EqualsCI("84754E")) query = externalFilaments.Where(x => x.Id.EqualsCI("bambulab_pla_bronze_1000_175_n")).AsQueryable();
 
